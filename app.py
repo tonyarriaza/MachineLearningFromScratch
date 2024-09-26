942cd94e3dfbaf78ccc9b303b065783aadf62906
tony <arriazatony@gmail.com>
2024-09-26 10:14:35 -0400

    basic website

--- app.py
+++ app.py
@@ -1,50 +1,87 @@
-from flask import Flask, abort, render_template_string
-import nbformat
-from nbconvert import HTMLExporter
-import os
-
-app = Flask(__name__)
-
-# Path to the directory containing notebooks
-NOTEBOOK_DIR = os.path.dirname(os.path.abspath(__file__))
-from flask import Flask, render_template
+from flask import Flask, render_template_string, send_from_directory, abort
 import subprocess
 import os
 
 app = Flask(__name__)
 
+# Keep track of the last modification time for the homepage
+last_modified_time = None
+
 @app.route('/')
-def index():
-    # Convert the LaTeX document to HTML using Pandoc
-    subprocess.run(['pandoc', '-s', 'templates/homepage.tex', '-o', 'templates/homepage.html'], check=True)
+def homepage():
+    global last_modified_time
+    tex_file = 'homepage.tex'
+    html_output = 'homepage.html'
 
-    # Serve the generated HTML
-    return render_template('output.html')
+    # Get the current modification time of the .tex file
+    current_modified_time = os.path.getmtime(tex_file)
 
-# @app.route('/')
-# def index():
-#     # List all .ipynb files in the directory
-#     notebooks = [f for f in os.listdir(NOTEBOOK_DIR) if f.endswith('.ipynb')]
-#     notebook_links = ''.join([
-#         f'<li><a href="/notebook/{nb}">{nb}</a></li>' for nb in notebooks
-#     ])
-#     return f'''
-#     <h1>Available Notebooks</h1>
-#     <ul>
-#         {notebook_links}
-#     </ul>
-#     '''
+    # Only run Pandoc if the file has changed
+    if last_modified_time is None or current_modified_time > last_modified_time:
+        last_modified_time = current_modified_time
+        subprocess.run([
+            'pandoc',
+            tex_file,
+            '-s',
+            '-o',
+            html_output,
+            '--mathjax'
+        ], check=True)
 
-# @app.route('/notebook/<path:notebook_name>')
-# def render_notebook(notebook_name):
-#     notebook_path = os.path.join(NOTEBOOK_DIR, notebook_name)
-#     if not os.path.exists(notebook_path):
-#         abort(404)
-#     with open(notebook_path, 'r', encoding='utf-8') as f:
-#         nb = nbformat.read(f, as_version=4)
-#     html_exporter = HTMLExporter()
-#     (body, _) = html_exporter.from_notebook_node(nb)
-#     return body
+    # Read the generated HTML content
+    with open(html_output, 'r', encoding='utf-8') as f:
+        html_content = f.read()
+
+    # Render the HTML content directly
+    return render_template_string(html_content)
+
+# Route to serve notebooks as HTML
+@app.route('/notebooks/<path:filename>')
+def serve_notebook(filename):
+    # Sanitize the filename to prevent security issues
+    from werkzeug.utils import secure_filename
+    filename = secure_filename(filename)
+
+    notebook_path = os.path.join('notebooks', filename)
+    if not os.path.exists(notebook_path):
+        abort(404)
+
+    # Generate the output HTML filename
+    base_name = os.path.splitext(filename)[0]
+    html_filename = base_name + '.html'
+    html_output_path = os.path.join('notebooks', html_filename)
+
+    # Convert the notebook to HTML using nbconvert
+    try:
+        subprocess.run([
+            'jupyter',
+            'nbconvert',
+            '--to',
+            'html',
+            filename,  # Updated to remove 'notebooks/' prefix
+            '--output',
+            html_filename
+        ], check=True, cwd='notebooks', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        # Decode and print the error message
+        error_message = e.stderr.decode()
+        print("Error converting notebook:", error_message)
+        abort(500)
+
+    # Read the generated HTML content
+    with open(html_output_path, 'r', encoding='utf-8') as f:
+        html_content = f.read()
+
+    # Optionally, delete the generated HTML file after rendering
+    # os.remove(html_output_path)
+
+    # Render the HTML content directly
+    return render_template_string(html_content)
+
+# Route to serve images
+@app.route('/static/images/<path:filename>')
+def serve_image(filename):
+    return send_from_directory('static/images', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
